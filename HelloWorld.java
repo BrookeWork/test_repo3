92af9af844f5ae60d218edcca8a1bda9db3ab622
BrookeWork <landry@cpp.edu>
2025-09-02 21:47:03 -0700

    test repo 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-		//hello
-        System.out.println("Hello, World!");
+		System.out.println("Hello, World!");
 		System.out.println("Hey, World!");
 		System.out.println("Hi, World!");
 		int x = 5;
@@ -9,7 +8,6 @@ public class HelloWorld {
 		x = 10;
 		System.out.println("x == 10");
 		sayNumber(x);
-		System.out.println("commit");
 	}
 	public static void sayNumber(int x) {
 		System.out.println(x);
